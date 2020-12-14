9e4283801cc77165adc2f03d77c277ce5c0789a1
Jitendra <jeetscmaker@gmail.com>
2020-12-14 23:27:00 +0530

    binary to decimal conversion of number.

--- test/test.js
+++ test/test.js
@@ -50,6 +50,10 @@ bitwiseOps = function () {
 }
 
 //Converting Decimal to Binary
-function dec2bin(dec){
+function dec2bin(dec) {
     return (dec >>> 0).toString(2);
-  }
+}
+//Converting Binary to Decimal
+function bin2dec(bin) {
+    return parseInt(bin, 2).toString(10);
+}
