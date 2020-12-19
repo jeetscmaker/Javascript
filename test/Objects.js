363062fcfc9db6dec54ae746a0483888e5471064
Jitendra <jeetscmaker@gmail.com>
2020-12-19 23:58:14 +0530

    typeof 'undefined' for uninitialized variable.

--- test/Objects.js
+++ test/Objects.js
@@ -2,3 +2,6 @@ var person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue" };
 console.log("Person name: " + person.firstName + " " + person.lastName);
 console.log("Type of firstname: " + typeof person.firstName );
 console.log("Type of age: " + typeof person.age);
+
+var car; // undefined type of variable car.
+console.log("Type of car is: " + typeof car);
