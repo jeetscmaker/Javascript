aaacee24779fb5cfc3f4efd5b840d6fec20e41fa
Jitendra <jeetscmaker@gmail.com>
2020-12-19 23:51:08 +0530

    typeof in javascript

--- test/Objects.js
+++ test/Objects.js
@@ -1,2 +1,4 @@
 var person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue" };
 console.log("Person name: " + person.firstName + " " + person.lastName);
+console.log("Type of firstname: " + typeof person.firstName );
+console.log("Type of age: " + typeof person.age);
