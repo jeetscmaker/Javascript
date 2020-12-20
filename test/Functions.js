8fe1513a939dcb20f41d844cc13c30986347a7dc
Jitendra <jeetscmaker@gmail.com>
2020-12-20 13:48:24 +0530

    template string used instead of normal string.

--- test/Functions.js
+++ test/Functions.js
@@ -1,3 +1,9 @@
+/*
+Function parameters are listed inside the parentheses () in the function definition.
+Function arguments are the values received by the function when it is invoked.
+Inside the function, the arguments (the parameters) behave as local variables.
+*/
+
 var sum = function (a, b) {
     console.log("Inside sum() function...");
     return a + b;
@@ -8,7 +14,7 @@ function subtract(a, b) {
     return a - b;
 }
 
-console.log("Type of sum function is: " + typeof sum);
-console.log("Type of subtract function is: " + typeof subtract);
-console.log("Output of sum function with a=15, b=10 is: " + sum(15, 10));
-console.log("Type of subtract function with a=15, b=10 is: " + subtract(15, 10));
+console.log(`Type of sum function is: ${typeof sum}`);
+console.log(`Type of subtract function is: ${typeof subtract}`);
+console.log(`Output of sum function with a=15, b=10 is: ${sum(15, 10)}`);
+console.log(`Output of subtract function with a=15, b=10 is: ${subtract(15, 10)}`);
