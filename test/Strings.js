2d2b01249dab2d4c66480413ac74e9e35eacc363
Jitendra <jeetscmaker@gmail.com>
2020-12-26 00:51:47 +0530

    String in Javascript

---
+++ test/Strings.js
@@ -0,0 +1,10 @@
+// Note the difference between (x==y) and (x===y).
+// Comparing two JavaScript objects will always return false.
+var x = "John";             
+var y = new String("John");
+console.log("x == y is: " + (x == y)); // returns true because the values of x and y are same.
+console.log("x === y is: " + (x === y)); // returns false because the type of x and y are different.
+
+var x = new String("John");             
+var y = new String("John");
+console.log("x == y is: " + (x == y)); // returns false because x and y are totally different objects.
