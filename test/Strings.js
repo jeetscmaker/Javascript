8361c3ecd2b82e772324c5d0044b4e52ae5b498c
Jitendra <jeetscmaker@gmail.com>
2020-12-26 23:40:20 +0530

    string methods

--- test/Strings.js
+++ test/Strings.js
@@ -8,3 +8,5 @@ console.log("x === y is: " + (x === y)); // returns false because the type of x 
 var x = new String("John");             
 var y = new String("John");
 console.log("x == y is: " + (x == y)); // returns false because x and y are totally different objects.
+
+/*----String Methods-------*/
