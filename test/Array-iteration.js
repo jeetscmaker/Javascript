ee1834d4174a5a85e0d2137d8dba85f528320fe4
Jitendra <jeetscmaker@gmail.com>
2021-01-09 21:39:25 +0530

    map() on Arrays.

--- test/Array-iteration.js
+++ test/Array-iteration.js
@@ -5,3 +5,15 @@ numbers.forEach(myFunction);
 function myFunction(value) {
   console.log(value);
 }
+
+/*The map() method creates a new array by performing a function on each array element.
+The map() method does not execute the function for array elements without values.
+The map() method does not change the original array.
+This example multiplies each array value by 2:
+*/
+var numbers1 = [45, 4, 9, 16, 25];
+var numbers2 = numbers1.map(myFunction).forEach(e => console.log(e));
+
+function myFunction(value, index, array) {
+  return value * 2;
+}
