918acd2012c6e0213d87ef031c46abf78f16ae1e
Jitendra <jeetscmaker@gmail.com>
2021-01-09 21:43:06 +0530

    Filter() in Arrays

--- test/Array-iteration.js
+++ test/Array-iteration.js
@@ -1,8 +1,8 @@
 //The forEach() method calls a function (a callback function) once for each array element.
 var numbers = [45, 4, 9, 16, 25];
-numbers.forEach(myFunction);
+numbers.forEach(myFunctionIterate);
 
-function myFunction(value) {
+function myFunctionIterate(value) {
   console.log(value);
 }
 
@@ -12,8 +12,18 @@ The map() method does not change the original array.
 This example multiplies each array value by 2:
 */
 var numbers1 = [45, 4, 9, 16, 25];
-var numbers2 = numbers1.map(myFunction).forEach(e => console.log(e));
+var numbers2 = numbers1.map(myFunctionMap).forEach(e => console.log(e));
 
-function myFunction(value, index, array) {
+function myFunctionMap(value, index, array) {
   return value * 2;
 }
+
+/*The filter() method creates a new array with array elements that passes a test.
+This example creates a new array from elements with a value larger than 18:
+*/
+var numbers = [45, 4, 9, 16, 25];
+var over18 = numbers.filter(myFunctionFilter).forEach(e => console.log(e));
+
+function myFunctionFilter(value, index, array) {
+  return value > 18;
+} 
