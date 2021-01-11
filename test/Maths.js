ec1e72c0d3f66f4ee82f2a6c78483f58863a8eb8
Jitendra <jeetscmaker@gmail.com>
2021-01-11 23:29:41 +0530

    Some more math methods

--- test/Maths.js
+++ test/Maths.js
@@ -1,1 +1,50 @@
 console.log("PI is " + Math.PI);
+
+//Math.round(x) returns the value of x rounded to its nearest integer:
+Math.round(4.7);    // returns 5
+Math.round(4.4);    // returns 4
+
+//Math.pow(x, y) returns the value of x to the power of y:
+console.log(Math.pow(8, 2));  // returns 64
+
+//Math.sqrt(x) returns the square root of x:
+Math.sqrt(64);      // returns 8
+
+//Math.abs(x) returns the absolute (positive) value of x:
+Math.abs(-4.7);     // returns 4.7
+
+//Math.ceil(x) returns the value of x rounded up to its nearest integer:
+Math.ceil(4.4);     // returns 5
+
+//Math.floor(x) returns the value of x rounded down to its nearest integer:
+Math.floor(4.7);    // returns 4
+
+//Math.sin(x) returns the sine (a value between -1 and 1) of the angle x (given in radians).
+
+/*If you want to use degrees instead of radians, you have to convert degrees to radians:
+Angle in radians = Angle in degrees x PI / 180.
+*/
+Math.sin(90 * Math.PI / 180);     // returns 1 (the sine of 90 degrees)
+
+/*Math.cos(x) returns the cosine (a value between -1 and 1) of the angle x (given in radians).
+If you want to use degrees instead of radians, you have to convert degrees to radians:
+Angle in radians = Angle in degrees x PI / 180.
+*/
+Math.cos(0 * Math.PI / 180);     // returns 1 (the cos of 0 degrees)
+
+//Math.min() and Math.max() can be used to find the lowest or highest value in a list of arguments:
+Math.min(0, 150, 30, 20, -8, -200);  // returns -200
+Math.max(0, 150, 30, 20, -8, -200);  // returns 150
+
+//Math.random() returns a random number between 0 (inclusive), and 1 (exclusive):
+console.log("Random value " + Math.random());    // returns a random number
+
+//JavaScript provides 8 mathematical constants that can be accessed with the Math object:
+Math.E        // returns Euler's number
+Math.PI       // returns PI
+Math.SQRT2    // returns the square root of 2
+Math.SQRT1_2  // returns the square root of 1/2
+Math.LN2      // returns the natural logarithm of 2
+Math.LN10     // returns the natural logarithm of 10
+Math.LOG2E    // returns base 2 logarithm of E
+Math.LOG10E   // returns base 10 logarithm of E
