52b69389fe3a5ff1741a3d56cfd7d102f8c03c01
Jitendra <jeetscmaker@gmail.com>
2021-01-18 23:44:56 +0530

    this keyword inside Arrow function of JS

--- test/this_keyword.js
+++ test/this_keyword.js
@@ -48,3 +48,13 @@ var person2 = {
   lastName: "Doe",
 }
 person1.fullName.call(person2);  // Will return "John Doe" 
+
+/*
+The handling of this is also different in arrow functions compared to regular functions.
+
+In short, with arrow functions there are no binding of this.
+
+In regular functions the this keyword represented the object that called the function, which could be the window, the document, a button or whatever.
+
+With arrow functions the this keyword always represents the object that defined the arrow function.
+*/
