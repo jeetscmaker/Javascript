e4d2b4affe5fc6502ccdf5608cc134e5d1e4fccd
Jitendra <jeetscmaker@gmail.com>
2021-01-19 23:19:09 +0530

    Class in Javascript

---
+++ test/Class.js
@@ -0,0 +1,13 @@
+/*
+ECMAScript 2015, also known as ES6, introduced JavaScript Classes.
+JavaScript Classes are templates for JavaScript Objects.
+
+Use the keyword 'class' to create a class.
+Always add a method named constructor():
+*/
+class Car {
+    constructor(name, year) {
+      this.name = name;
+      this.year = year;
+    }
+  }
