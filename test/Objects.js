65b0f08084db86bef8a131fc87d2d96b2dce7e44
Jitendra <jeetscmaker@gmail.com>
2021-01-23 23:32:38 +0530

    ES5 New Object Methods

--- test/Objects.js
+++ test/Objects.js
@@ -75,3 +75,40 @@ function Person(first, last, age, eyecolor) {
   Person.prototype.name = function() {
     return this.firstName + " " + this.lastName;
   };
+
+// --------- ES5 New Object Methods ----------
+// Adding or changing an object property
+Object.defineProperty(object, property, descriptor)
+
+// Adding or changing many object properties
+Object.defineProperties(object, descriptors)
+
+// Accessing Properties
+Object.getOwnPropertyDescriptor(object, property)
+
+// Returns all properties as an array
+Object.getOwnPropertyNames(object)
+
+// Returns enumerable properties as an array
+Object.keys(object)
+
+// Accessing the prototype
+Object.getPrototypeOf(object)
+
+// Prevents adding properties to an object
+Object.preventExtensions(object)
+
+// Returns true if properties can be added to an object
+Object.isExtensible(object)
+
+// Prevents changes of object properties (not values)
+Object.seal(object)
+
+// Returns true if object is sealed
+Object.isSealed(object)
+
+// Prevents any changes to an object
+Object.freeze(object)
+
+// Returns true if object is frozen
+Object.isFrozen(object)
