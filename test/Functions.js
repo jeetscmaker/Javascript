fcfc686788c595f5bb8c0e4b23637f048b6eea8a
Jitendra <jeetscmaker@gmail.com>
2021-01-25 00:06:54 +0530

    examples of apply() method.

--- test/Functions.js
+++ test/Functions.js
@@ -60,5 +60,19 @@ The call() method takes arguments separately.
 The apply() method takes arguments as an array.
 The apply() method is very handy if you want to use an array instead of an argument list.
 */
+person.fullName.apply(person1, ["Oslo", "Norway"]);
+person.fullName.call(person1, "Oslo", "Norway");
 
+//You can find the largest number (in a list of numbers) using the Math.max() method:
+Math.max(1,2,3);  // Will return 3 
+//Since JavaScript arrays do not have a max() method, you can apply the Math.max() method instead.
+Math.max.apply(null, [1,2,3]); // Will also return 3 
 
+//The first argument (null) does not matter. It is not used in this example.
+//These examples will give the same result:
+Math.max.apply(Math, [1,2,3]); // Will also return 3 
+Math.max.apply(" ", [1,2,3]); // Will also return 3 
+Math.max.apply(0, [1,2,3]); // Will also return 3 
+
+//In JavaScript strict mode, if the first argument of the apply() method is not an object, 
+//it becomes the owner (object) of the invoked function. In "non-strict" mode, it becomes the global object.
