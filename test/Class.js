cab63c2f0df7d3aa59ad3e7545aaec0f23add343
Jitendra <jeetscmaker@gmail.com>
2021-01-25 23:58:18 +0530

    Extend keyword inheritance

--- test/Class.js
+++ test/Class.js
@@ -47,11 +47,39 @@ class ClassName {
 let myCar = new Car("Ford", 2014);
 console.log("My car is " + myCar.age() + " years old.");
 
-class Student{
-    constructor(id){
+class Student {
+    constructor(id) {
         this.roll = id;
     }
 }
 
 let s = new Student(15);
 console.log(`The roll number is ${s.roll}`);
+
+/*
+Class Inheritance
+-----------------
+To create a class inheritance, use the extends keyword.
+A class created with a class inheritance inherits all the methods from another class.
+Create a class named "Model" which will inherit the methods from the "Car" class:
+*/
+class Car {
+    constructor(brand) {
+        this.carname = brand;
+    }
+    present() {
+        return 'I have a ' + this.carname;
+    }
+}
+class Model extends Car {
+    constructor(brand, mod) {
+        super(brand);
+        this.model = mod;
+    }
+    show() {
+        return this.present() + ', it is a ' + this.model;
+    }
+}
+
+let myCar = new Model("Ford", "Mustang");
+document.getElementById("demo").innerHTML = myCar.show();
