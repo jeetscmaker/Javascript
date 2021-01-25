fe0a4568e700f2e6b19ad3fd74087c4851cbf0f9
Jitendra <jeetscmaker@gmail.com>
2021-01-26 00:41:15 +0530

    static methods in class in JS

--- test/Class.js
+++ test/Class.js
@@ -180,3 +180,41 @@ Note: For other declarations, like functions, you will NOT get an error when you
 it is declared, because the default behavior of JavaScript declarations are hoisting 
 (moving the declaration to the top).
 */
+//===============STATIC METHODS===========================
+/*
+Static class methods are defined on the class itself.
+
+You cannot call a static method on an object, only on an object class.
+
+Example
+class Car {
+  constructor(name) {
+    this.name = name;
+  }
+  static hello() {
+    return "Hello!!";
+  }
+}
+
+let myCar = new Car("Ford");
+
+// You can calll 'hello()' on the Car Class:
+document.getElementById("demo").innerHTML = Car.hello();
+
+// But NOT on a Car Object:
+// document.getElementById("demo").innerHTML = myCar.hello();
+// this will raise an error.
+
+If you want to use the myCar object inside the static method, you can send it as a parameter:
+Example
+class Car {
+  constructor(name) {
+    this.name = name;
+  }
+  static hello(x) {
+    return "Hello " + x.name;
+  }
+}
+let myCar = new Car("Ford");
+document.getElementById("demo").innerHTML = Car.hello(myCar);
+*/
