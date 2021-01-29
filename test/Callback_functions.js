e73d20b2754e52d7287bfba152e2caf0174c61f9
Jitendra <jeetscmaker@gmail.com>
2021-01-29 23:28:53 +0530

    Javascript Promise

--- test/Callback_functions.js
+++ test/Callback_functions.js
@@ -33,3 +33,18 @@ setTimeout(myFunction, 3000);
 function myFunction() {
   console.log("I love You !!");
 }
+
+// JavaScript Promise Object
+// A JavaScript Promise object contains both the producing code and calls to the consuming code:
+let myPromise = new Promise(function(myResolve, myReject) {
+  // "Producing Code" (May take some time)
+  
+    myResolve(); // when successful
+    myReject();  // when error
+  });
+  
+  // "Consuming Code" (Must wait for a fulfilled Promise)
+  myPromise.then(
+    function(value) { /* code if successful */ },
+    function(error) { /* code if some error */ }
+  );
