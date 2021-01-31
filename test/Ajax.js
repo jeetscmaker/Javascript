f386bc069b7b332dec84385ae8047597fd71a70c
Jitendra <jeetscmaker@gmail.com>
2021-01-31 23:22:26 +0530

    How to send Ajax request

---
+++ test/Ajax.js
@@ -0,0 +1,4 @@
+//The XMLHttpRequest object is used to exchange data with a server.
+//To send a request to a server, we use the open() and send() methods of the XMLHttpRequest object:
+xhttp.open("GET", "url/ajax_info.txt", true);
+xhttp.send();
