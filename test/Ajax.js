1ffe3b78ff3b6304bff576d0bec650956b932f4e
Jitendra <jeetscmaker@gmail.com>
2021-01-31 23:57:13 +0530

    POST request

--- test/Ajax.js
+++ test/Ajax.js
@@ -23,3 +23,12 @@ However, always use POST requests when:
     Sending a large amount of data to the server (POST has no size limitations).
     Sending user input (which can contain unknown characters), POST is more robust and secure than GET.
 */
+//If you want to send information with the GET method, add the information to the URL:
+xhttp.open("GET", "demo_get2.asp?fname=Henry&lname=Ford", true);
+xhttp.send(); 
+
+//To POST data like an HTML form, add an HTTP header with setRequestHeader(). Specify the data 
+//you want to send in the send() method:
+xhttp.open("POST", "ajax_test.asp", true);
+xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+xhttp.send("fname=Henry&lname=Ford"); 
