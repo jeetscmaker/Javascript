140d562f6e1bf393a354fb90e8d0f369a7a01d80
Jitendra <jeetscmaker@gmail.com>
2022-01-20 10:33:59 -0800

    few methods

--- maps_in_JS.js
+++ maps_in_JS.js
@@ -14,3 +14,36 @@ Essential Map Methods:
 Property 	Description
 	size ---	Returns the number of elements in a Map
 */
+
+// Create a Map
+const fruits_and_their_quantity = new Map([
+  ["apples", 500],
+  ["bananas", 300],
+  ["oranges", 200]
+]);
+
+// You can add elements to a Map with the set() method:
+// Create a Map
+const fruits = new Map(); // in Java, it is, Map<String, Integer> fruits = new LinkedHashMap<String, Integer>();
+
+// Set Map Values
+fruits.set("apples", 500); // in Java it is, fruits.put("apples", 500);
+fruits.set("bananas", 300);
+fruits.set("oranges", 200);
+
+// The set() method can also be used to change existing Map values.
+fruits.set("apples", 100); 
+
+// The get() method gets the value of a key in a Map:
+fruits.get("apples");    // Returns 500
+
+// The size property returns the number of elements in a Map:
+fruits.size; // returns 3
+
+// The delete() method removes a Map element:
+fruits.delete("apples");
+
+// The has() method returns true if a key exists in a Map:
+fruits.has("apples"); // returns false because we just deleted ["apples", 100]
+
+
