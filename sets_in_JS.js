7fc55f25b7bb732cd431401705f054d71ab52dd3
Jitendra <jeetscmaker@gmail.com>
2022-01-19 22:14:48 -0800

    methods of set data structure.

--- sets_in_JS.js
+++ sets_in_JS.js
@@ -13,3 +13,24 @@ You can create a JavaScript Set by:
     Create a new Set and use add() to add variables
 */
 const letters = new Set(["a","b","c", "a"]); // creates a set of size 3 containing a,b,c.
+
+// Create a Set
+const letters = new Set();
+
+// Add Values to the Set
+letters.add("a");
+letters.add("b");
+letters.add("c");
+
+/*
+Essential Set Methods
+------------------------
+new Set() ---	Creates a new Set
+add()	  ---   Adds a new element to the Set
+delete()  ---   Removes an element from a Set
+has()	  ---   Returns true if a value exists in the Set
+forEach() ---	Invokes a callback for each element in the Set
+values()  ---	Returns an iterator with all the values in a Set
+Property  ---	Description
+size	  ---   Returns the number of elements in a Set
+*/
